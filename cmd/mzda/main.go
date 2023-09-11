0fae0b72e9d2880cf2b3f0176ce3c23997cd9de5
Ilia Malinin <malinin.ilja@gmail.com>
2023-09-11 23:59:20 +0300

    Testing of endpoints (WIP)

--- cmd/mzda/main.go
+++ cmd/mzda/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/chi/v5"
 	"log"
+	"mzda/internal/auth/handlers"
 	"mzda/internal/storage/db/postgres"
-	"mzda/internal/storage/models/mzda"
+	"net/http"
 )
 
 const (
@@ -12,6 +13,7 @@ const (
 	envDev   = "dev"
 	envProd  = "prod"
 	svc      = "AUTH"
+	apiVer   = "1.0"
 )
 
 func main() {
@@ -29,68 +31,13 @@ func main() {
 		log.Fatal("Couldn't connect to database")
 	}
 
-	usrDTO1 := mzda.UserDTO{
-		Username: "TestUser1",
-		Pwd:      "1234",
-		Email:    "test1@test.com",
-	}
-
-	usrDTO2 := mzda.UserDTO{
-		Username: "TestUser2",
-		Pwd:      "4321",
-		Email:    "test2@test.com",
-	}
-
-	err = storage.AddUser(&usrDTO1)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = storage.AddUser(&usrDTO2)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	usr, err := storage.UserByName("TestUser1")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(usr)
-
-	usrID := usr.ID
-
-	usr, err = storage.UserByEmail("test2@test.com")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(usr)
-
-	usr, err = storage.UserByID(usrID)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(usr)
-
-	err = storage.DeleteUser(usr)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	usr, err = storage.UserByEmail("test2@test.com")
-	usr.Username = "TestUser"
-
-	err = storage.UpdateUser(usr)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	usr, err = storage.UserByName("TestUser")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(usr)
-
 	// TODO Init server
+	router := chi.NewRouter()
+	router.Post("/signin", handlers.SignIn(storage, storage))
+	router.Post("/signup", handlers.SignUp(storage))
 
-	// TODO Run app
+	err := http.ListenAndServe(":32000", router)
+	if err != nil {
+		return
+	}
 }
