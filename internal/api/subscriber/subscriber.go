5418ee6c878e451f79793ba410a5cfac26427049
Ilia Malinin <malinin.ilja@gmail.com>
2023-10-04 17:31:28 +0300

    Defined endpoints for Subscriber

---
+++ internal/api/subscriber/subscriber.go
@@ -0,0 +1,141 @@
+package subscriber
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"log"
+	"mzda/internal/svc/subscriber"
+	"net/http"
+	"strconv"
+)
+
+func NewSubscriber(svc subscriber.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "internal/api/subscriber/NewSubscriber"
+		statusCode, err := svc.AddSubscriber(r)
+		if err != nil {
+			log.Println(fmt.Errorf("%s %v", fn, err))
+			http.Error(w, err.Error(), statusCode)
+			return
+		}
+		w.WriteHeader(statusCode)
+		return
+	}
+}
+
+func GetSubscriber(svc subscriber.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "internal/api/subscriber/GetSubscriber"
+
+		param := chi.URLParam(r, "id")
+		if param == "" {
+			err := fmt.Errorf("%s %v", fn, "id is empty")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		id, err := strconv.Atoi(param)
+		if err != nil {
+			err := fmt.Errorf("%s %v", fn, "id is not a number")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		response, statusCode, err := svc.SubscriberByID(id)
+		if err != nil {
+			log.Println(fmt.Errorf("%s %v", fn, err))
+			http.Error(w, err.Error(), statusCode)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(response)
+		if err != nil {
+			log.Println(fmt.Errorf("%s %v", fn, err))
+			http.Error(w, "failed to send data", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+}
+
+func GetSubscribersByUserID(svc subscriber.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "internal/api/subscriber/GetSubscribers"
+
+		param := chi.URLParam(r, "id")
+		if param == "" {
+			err := fmt.Errorf("%s %v", fn, "id is empty")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		id, err := strconv.Atoi(param)
+		if err != nil {
+			err := fmt.Errorf("%s %v", fn, "id is not a number")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		response, statusCode, err := svc.ListSubscribersByUserID(id)
+		if err != nil {
+			log.Println(fmt.Errorf("%s %v", fn, err))
+			http.Error(w, err.Error(), statusCode)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(response)
+		if err != nil {
+			log.Println(fmt.Errorf("%s %v", fn, err))
+			http.Error(w, "failed to send data", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+}
+
+func UpdateSubscriber(svc subscriber.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "internal/api/subscriber/UpdateSubscriber"
+		statusCode, err := svc.UpdateSubscriber(r)
+		if err != nil {
+			log.Println(fmt.Errorf("%s %v", fn, err))
+			http.Error(w, err.Error(), statusCode)
+			return
+		}
+		w.WriteHeader(statusCode)
+		return
+	}
+}
+
+func DeleteSubscriber(svc subscriber.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "internal/api/subscriber/DeleteSubscriber"
+
+		param := chi.URLParam(r, "id")
+		if param == "" {
+			err := fmt.Errorf("%s %v", fn, "id is empty")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		id, err := strconv.Atoi(param)
+		if err != nil {
+			err := fmt.Errorf("%s %v", fn, "id is not a number")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		statusCode, err := svc.DeleteSubscriberByID(id, r)
+		if err != nil {
+			log.Println(fmt.Errorf("%s %v", fn, err))
+			http.Error(w, err.Error(), statusCode)
+			return
+		}
+		w.WriteHeader(statusCode)
+		return
+	}
+}
--- internal/svc/subscriber/subscriberSvc.go
+++ internal/svc/subscriber/subscriberSvc.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Service interface {
-	AddSubscriber(req http.Request) (statusCode int, err error)
-	SubscriberByID(id int) (sub *models.Subscriber, statusCode int, err error)
-	ListSubscribersByUserID(usrID int) (subs []*models.Subscriber, statusCode int, err error)
-	UpdateSubscriber(req http.Request) (statusCode int, err error)
-	DeleteSubscriberByID(id int) (statusCode int, err error)
+	AddSubscriber(req *http.Request) (statusCode int, err error)
+	SubscriberByID(id int) (sub []byte, statusCode int, err error)
+	ListSubscribersByUserID(usrID int) (subs []byte, statusCode int, err error)
+	UpdateSubscriber(req *http.Request) (statusCode int, err error)
+	DeleteSubscriberByID(id int, req *http.Request) (statusCode int, err error)
 }
 
 type Svc struct {
